a64965bdab1bb466689602ba801b313515083daf
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 19:06:47 -0400

    worked server.js

--- server.js
+++ server.js
@@ -9,3 +9,12 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: { maxAge: 36000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
