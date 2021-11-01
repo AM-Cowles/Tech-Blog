045695b278fc665bc0e1d53df77828bbecb09d4e
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 20:58:42 -0400

    finished add-post.js

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -10,4 +10,16 @@ async function newFormHandler(event) {
         title,
         content
     }),
-    
+    headers: {
+        'Content-Type': 'application/json'
+    }
+    });
+
+    if (response.ok) {
+    document.location.replace('/dashboard');
+    } else {
+    alert(response.statusText);
+    }
+};
+
+document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);
