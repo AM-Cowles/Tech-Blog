88a1e8add545d79ca732a67e056faff3326ce446
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 20:48:48 -0400

    started login.js

---
+++ public/javascript/login.js
@@ -0,0 +1,16 @@
+//  login submission 
+
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+    const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+        username,
+        password
+        }),
+        
