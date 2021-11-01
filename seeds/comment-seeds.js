db28494af9d2f39a08e1c392e5ad73ceb20ddb5d
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 20:32:10 -0400

    started comment-seeds.js

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,12 @@
+const { Comment } = require('../models');
+
+const commentData = [{
+        comment_text: "Lorem ipsum dolor sit amet",
+        user_id: 1,
+        post_id: 1
+    },
+    {
+        comment_text: "consectetur adipiscing elit",
+        user_id: 2,
+        post_id: 2
+    },
