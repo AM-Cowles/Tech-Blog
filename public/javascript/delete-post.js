8903d64107a40f686a2d0809564daeb4c08e4e6d
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 20:53:06 -0400

    started delete-post.js

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,13 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+        post_id: id
+        }),
+        
