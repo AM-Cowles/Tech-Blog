6ccbb05b45bd217e890dd252134844a4d5f58b12
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 21:29:52 -0400

    worked post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -71,3 +71,36 @@ router.get('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id
+        })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', withAuth, (req, res) => {
+    Post.update({
+            title: req.body.title,
+            content: req.body.content
+        }, {
+            where: {
+                id: req.params.id
+            }
+        }).then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
