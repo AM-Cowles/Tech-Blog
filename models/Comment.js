d46ce9cf245b992d71cc7aa9cc87018102c75050
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 21:08:22 -0400

    started Comment.js

---
+++ models/Comment.js
@@ -0,0 +1,17 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+class Comment extends Model {}
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    comment_text: {
+        type: DataTypes.STRING,
+        validate: {
+
+            len: [3]
+        }
+    },
+    
