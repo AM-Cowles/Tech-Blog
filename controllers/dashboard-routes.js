240aa6b72480023b16d003ac1a3f000f36cb56b9
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 21:21:04 -0400

    worked dashboard-routes.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+
 router.get('/', withAuth, (req, res) => {
     Post.findAll({
             where: {
@@ -36,3 +37,34 @@ router.get('/', withAuth, (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            
