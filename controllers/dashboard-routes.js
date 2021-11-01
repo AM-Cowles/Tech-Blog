828445038a2a5fbf27db31a0460150b9dc55941e
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 21:19:07 -0400

    started dashboard-routes.js

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            
