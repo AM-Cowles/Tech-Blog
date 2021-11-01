05674de8354acdae74b2c40983289775d44fbcf6
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 21:00:35 -0400

    finished index.js

--- models/index.js
+++ models/index.js
@@ -14,3 +14,19 @@ Comment.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: "cascade"
 });
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: "cascade"
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: "cascade"
+})
+module.exports = { User, Post, Comment };
