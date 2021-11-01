07efe61e90f7e36537827a95ab6a73d79eac0289
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 20:31:08 -0400

    finished index.js

--- seeds/index.js
+++ seeds/index.js
@@ -3,3 +3,13 @@ const seedPosts = require('./post-seeds');
 const seedComments = require('./comment-seeds');
 
 const sequelize = require('../config/connection');
+
+const seedAll = async() => {
+    await sequelize.sync({ force: true });
+    await seedUsers();
+    await seedPosts();
+    await seedComments();
+    process.exit(0);
+};
+
+seedAll();
