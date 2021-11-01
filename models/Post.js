9ad41a612e10bc5f57229adf2883208205fad248
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 21:07:23 -0400

    finished Post.js

--- models/Post.js
+++ models/Post.js
@@ -30,4 +30,12 @@ Post.init(
         }
     }
     },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+    );
     
+    module.exports = Post;
