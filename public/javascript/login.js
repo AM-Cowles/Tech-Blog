0c25e33c57f62d89173b4c3753344fdaf9e22d6d
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-11-02 17:49:42 -0400

    editted login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,5 +1,6 @@
 //  login submission 
 
+
 async function loginFormHandler(event) {
     event.preventDefault();
 
@@ -7,21 +8,22 @@ async function loginFormHandler(event) {
     const password = document.querySelector('#password-login').value.trim();
 
     if (username && password) {
-    const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-        username,
-        password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-    });
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
 
-    if (response.ok) {
-    document.location.replace('/dashboard');
-    } else {
-    alert(response.statusText);
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
     }
 }
-}
+
 
 document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
