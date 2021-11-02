b87a61d02e830510352d886389fc50b400ccdbd9
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-11-02 17:50:50 -0400

    editted edit-post.js

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,26 +2,26 @@ async function editFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value.trim();
-    const content = document.querySelector('input[name="content"]').value.trim();
+    const content = document.querySelector('textarea[name="content"]').value.trim();
     console.log(title);
     console.log(content);
 
     const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
+        window.location.toString().split('/').length - 1
     ];
-    
+
     const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
-        post_id: id,
-        title,
-        content
+            post_id: id,
+            title,
+            content
         }),
         headers: {
-        'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         }
     });
-    
+
     if (response.ok) {
         document.location.replace('/dashboard/');
     } else {
