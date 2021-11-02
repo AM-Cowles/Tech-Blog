eed8514e8715913b06f06c0efd859e7084c90e8a
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-11-02 18:11:09 -0400

    user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+// imports user, post, and comment models to use with our routes
 const { User, Post, Comment } = require('../../models');
+
+// for /api/users, find all of this users data besides the password
+// if found, return it to the screen; otherwise - drive an error
+// 500 - internal Server Error - unexpected condition prevents request fulfill
 router.get('/', (req, res) => {
     User.findAll({
             attributes: { exclude: ['[password'] }
@@ -11,6 +16,8 @@ router.get('/', (req, res) => {
         });
 });
 
+// get back to this later to better understand this process
+// when you click on this id, you get user info minus the password
 router.get('/:id', (req, res) => {
     User.findOne({
             attributes: { exclude: ['password'] },
@@ -54,6 +61,8 @@ router.get('/:id', (req, res) => {
         });
 });
 
+// when user signs up, we create a username and password in the request
+// for our user data, what we want to is log them into their session
 router.post('/', (req, res) => {
 
     User.create({
@@ -76,6 +85,13 @@ router.post('/', (req, res) => {
         });
 });
 
+// when user clicks login button, find their corresponding username
+// if there is no username with that data, let them know with a 400
+// 400 - Bad Request (seed MDN docs)
+// otherwise, check password against stored bcrypted password
+// if incorrect - 400
+//  otherwise, save their session
+// otherwise, 500 - Internal Server Error
 router.post('/login', (req, res) => {
     User.findOne({
             where: {
@@ -83,6 +99,7 @@ router.post('/login', (req, res) => {
             }
         }).then(dbUserData => {
             if (!dbUserData) {
+                // json message does not appear
                 res.status(400).json({ message: 'No user with that username!' });
                 return;
             }
@@ -106,9 +123,12 @@ router.post('/login', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// when user clicks logout, frontend script triggers to destroy sessions
 router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
+            // 204 - No Content success status
             res.status(204).end();
         });
     } else {
@@ -116,44 +136,5 @@ router.post('/logout', (req, res) => {
     }
 });
 
-router.put('/:id', (req, res) => {
-
-    User.update(req.body, {
-            individualHooks: true,
-            where: {
-                id: req.params.id
-            }
-        })
-        .then(dbUserData => {
-            if (!dbUserData[0]) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-
-});
-router.delete('/:id', (req, res) => {
-    User.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-        .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
 
 module.exports = router;
