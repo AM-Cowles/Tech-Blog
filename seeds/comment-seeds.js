bb5e8aa0c51674084ff72455db977011a781e7a8
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-11-02 17:19:08 -0400

    editted comment-seeds.js

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,22 +1,29 @@
 const { Comment } = require('../models');
 
 const commentData = [{
-        comment_text: "Lorem ipsum dolor sit amet",
+        comment_text: "I deserve to get hired.",
         user_id: 1,
         post_id: 1
     },
     {
-        comment_text: "consectetur adipiscing elit",
+        comment_text: "I hope this recruiter can help...",
         user_id: 2,
         post_id: 2
     },
     {
-        comment_text: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        comment_text: "I don't even know if I want to code anymore!",
         user_id: 3,
         post_id: 3
+    },
+    {
+        comment_text: "Don't let yourself get burnt out",
+        user_id: 4,
+        post_id: 4
     }
 ];
 
+// here we edit more than one record at a time by utilizing bulkCreate
+// we will use the above array of objects to seed our database with some sample comments
 const seedComments = () => Comment.bulkCreate(commentData);
 
 module.exports = seedComments;
